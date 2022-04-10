b62e2643130b784ea8ac4c1eb9ea2adf2f9ff807
ZEESHAN-latif <91531832+ZEESHAN-latif@users.noreply.github.com>
2022-04-10 16:25:38 +0500

    checking

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-    <div>
-    <Alert />
-  <Navbar title="TextUtils" aboutText="About Us"/>
-  <Btnsform />
-  </div>
+      <div>
+        <Alert />
+        <Navbar title="TextUtils" aboutText="About Us" />
+        <h2>this si checking</h2>
+        <Btnsform />
+      </div>
     </>
   );
 }
